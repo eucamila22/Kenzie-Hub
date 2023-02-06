559399280b21a5bb1fd0ee0951c1ed757987e137
Camila Gonçalves Domingos Tavares <105559819+eucamila22@users.noreply.github.com>
2023-02-06 11:44:29 -0300

    atualização de deploy

--- src/App.js
+++ src/App.js
@@ -3,44 +3,42 @@ import { Route, Routes } from 'react-router-dom'
 import RegisterPage from './pages/register'
 import DasboardPage from './pages/dashboard'
 import GlobalStyles, { Container } from './styles/globalStyles'
-import { ToastContainer } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import NotFound from './pages/NotFound'
 import { UserProvider } from './contexts/UserContext'
 import { TechProvider } from './contexts/TechContext'
 
 const App = () => {
+    return (
+        <Container>
+            <UserProvider>
+                <TechProvider>
+                    <Routes>
+                        <Route path='/' element={<LoginPage />} />
+                        <Route path='/register' element={<RegisterPage />} />
+                        <Route path='/dashboard' element={<DasboardPage />} />
+                        <Route path='*' element={<NotFound />} />
+                    </Routes>
+                </TechProvider>
+            </UserProvider>
 
-  return (
-    <Container>
-      
-      <UserProvider>        
-        <TechProvider>
-          <Routes>
-            <Route path='/' element={<LoginPage />}/>      
-            <Route path='/register' element={<RegisterPage />}/>
-            <Route path='/dashboard' element={<DasboardPage />}/>
-            <Route path='*' element={<NotFound />} />
-          </Routes>  
-        </TechProvider>
-      </UserProvider>
+            <GlobalStyles />
 
-      <GlobalStyles/>
-
-      <ToastContainer
-        position='bottom-right'
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme='light'
-      />
-    </Container>
-  )
+            <ToastContainer
+                position='bottom-right'
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme='light'
+            />
+        </Container>
+    )
 }
 
 export default App
